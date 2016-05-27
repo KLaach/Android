462fe7e72a32a60859179c1bf09fc16459c0ca4d
KLaach <k.laachouri@gmail.com>
2016-05-27 11:56:17 +0200

    Page SavePlace

--- MyFavoritePlaces/app/src/main/java/com/myfavoriteplaces/myfavoriteplaces/MainActivity.java
+++ MyFavoritePlaces/app/src/main/java/com/myfavoriteplaces/myfavoriteplaces/MainActivity.java
@@ -9,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
--- MyFavoritePlaces/app/src/main/java/com/myfavoriteplaces/myfavoriteplaces/SavePlaces.java
+++ MyFavoritePlaces/app/src/main/java/com/myfavoriteplaces/myfavoriteplaces/SavePlaces.java
@@ -8,13 +8,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavePlaces extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.save_places);
+
+        Spinner TypePlace = (Spinner) findViewById(R.id.TypePlace);
+        List ChoixType = new ArrayList();
+        ChoixType.add("Default");
+        ChoixType.add("Boîte");
+        ChoixType.add("Camping");
+        ChoixType.add("Cinéma");
+        ChoixType.add("Musée");
+        ChoixType.add("Restaurant");
+        ArrayAdapter adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                ChoixType
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        TypePlace.setAdapter(adapter);
     }
 }
